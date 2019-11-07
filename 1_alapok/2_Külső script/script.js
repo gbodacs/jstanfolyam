94e93cd137076567fe5b8250a053abe01ba93d8e
Gabor Bodacs <gbodacs@gmail.com>
2019-11-07 11:43:20 +0100

    Initial commit

---
+++ 1_alapok/2_Külső script/script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Megváltozott a szöveg.";
+}
+
+function myFunction2() {
+    document.getElementById("demo").innerHTML = "Ez egy másik változott szöveg.";
+}
