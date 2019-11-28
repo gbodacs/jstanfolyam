71c8bfd80e36f40450ef9931cc272898e6c7441d
Gabor <gbodacs@gmail.com>
2019-11-28 18:52:58 +0100

    Array fix + for fix

--- 3_Ciklusok/continue_break.js
+++ 3_Ciklusok/continue_break.js
@@ -3,14 +3,17 @@
         // The continue statement "jumps over" one iteration in the loop.
         for (i = 0; i < 100; i++) 
         {
+            console.log(i);
+            
             if (i == 1)
                 continue;
 
-            if (i == 2)
+            if (i == 3)
                 break;
 
             text += "The number is " + i + "<br>";
         }
+        vat o=1;
     }
 
 
