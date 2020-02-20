b0700516168b963e9201f711010da7672b39aa04
Gabor Bodacs <gbodacs@gmail.com>
2020-02-20 16:45:08 +0100

    NodeJS express tutorial

--- 4_Nodejs/2express/app.js
+++ 4_Nodejs/2express/app.js
@@ -1,17 +1,8 @@
 const express = require('express')
 const app = express()
-const io = require("socket.io")
+const port = 3000
 
-app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/bimserver', (req, res) => res.send('Hello BIMcloud!'))
 
-app.use(express.static('public'))
-
-app.get('/', (req,res)=> {
-    res.send('hello world!')
-})
-
-server = app.listen(3000)
-
-io.on('connection', (socket) => {
-    console.log('New user connected!')
-})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
