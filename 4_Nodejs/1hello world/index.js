74ac122df570279738ccf456c4e702f646cb7d93
Gabor <gbodacs@gmail.com>
2020-02-28 09:27:31 +0100

    fix for nodejs

--- 4_Nodejs/1hello world/index.js
+++ 4_Nodejs/1hello world/index.js
@@ -1,11 +1,11 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
-});
+var server = http.createServer(function(request, response) 
+    {
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.end("Hello World!");
+    }
+);
 
 var port = process.env.PORT || 1337;
 server.listen(port);
